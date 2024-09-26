e6db364483b82f5c5cfa70b4f65aafd1fe63a067
leonardo5055 <durandcaballero.leonardoet36@gmail.com>
2024-09-26 17:22:01 -0300

    nuevo diseño

--- src/App.jsx
+++ src/App.jsx
@@ -1,18 +1,10 @@
 import './App.css';
-import Nav from './componentes/estructura/Nav';
-import Login from './componentes/login/Login';
-import LoginPagina from './componentes/login/LoginPagina';
-import Historial from './componentes/paginas/Historial';
-import Home from './componentes/paginas/Home';
+import Login from './componentes/paginas/Login';
 
 function App() {
   return (
     <div>
-      <Nav></Nav>
-      <LoginPagina></LoginPagina>
-      {/* <Home></Home> */}
-      {/* <Historial></Historial> */}
-
+      <Login></Login>
     </div>
   );
 }
