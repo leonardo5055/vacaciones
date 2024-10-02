f60147620080de905ff1b4a5d29623c5422d05ea
martinlablanca <lablanca.martinet36@gmail.com>
2024-10-02 14:02:15 -0300

    termine el perfil, me falta editar perfil

--- src/componentes/paginas/Perfil.jsx
+++ src/componentes/paginas/Perfil.jsx
@@ -4,26 +4,35 @@ import Usuario from '../../img/usuario.png'
 function Perfil() {
     return (
         <div>
-            <h1 className='text-light text-center'>Perfil</h1>
-            <div className='d-flex justify-content-around'>
-                <div className='caja-negra'>
+            <h1 className='text-light text-center mt-3'>Perfil</h1>
+            <div className='d-flex justify-content-center  gap-5 m-5'>
+                <div className='caja-negra text-light text-center fs-4'>
                     <img src={Usuario} width={300} alt="" />
-                    <hr className='linea'/>
-                    <p className='text-light text-center'>Leonardo Durand Caballero</p>
-                    <hr className='linea'/>
-                    <p className='text-light'>Email:</p>
-                    <p className='text-light text-center'>leonardodurand@gmail.com</p>
-                    <hr className='linea'/>
-                    <p className='text-light'>Telefono:</p>
-                    <p className='text-light text-center'>11-5838-8355</p>
-                    <hr className='linea'/>
-                    <button className='btn-azul px-4 py-2 rounded-pill text-light border-0'>Editar perfil</button>
-
+                    <hr className='linea' />
+                    <p>Leonardo Durand Caballero</p>
+                    <hr className='linea' />
+                    <p className='d-flex justify-content-start'>Email:</p>
+                    <p>leonardodurand@gmail.com</p>
+                    <hr className='linea' />
+                    <div>
+                        <p className='d-flex justify-content-start'>Telefono:</p>
+                        <p>11-5838-8355</p>
+                    </div>
+                    <hr className='linea' />
+                    <button className='btn-azul px-4 py-2 rounded-pill text-light border-0 w-100'>Editar perfil</button>
                 </div>
-                <div className='caja-negra'>
-
+                <div className='caja-negra w-50 text-light fs-4 g-3'>
+                    <div>
+                        <p className='m-3'>Cargo: Jefe de desarrolladores</p>
+                        <hr className='linea' />
+                        <p className='m-3'>Area: Programacion</p>
+                        <hr className='linea' />
+                        <p className='m-3'>Años en la empresa: Seis años</p>
+                        <hr className='linea' />
+                        <p className='m-3'>Experiencia: Diez años</p>
+                        <hr className='linea' />
+                    </div>
                 </div>
-
             </div>
         </div>
     )
